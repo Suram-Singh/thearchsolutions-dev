b3d38e3a33e0080a834a584262833311512829e7
Suram Singh <suramsingh.dev@gmail.com>
2025-02-23 16:42:29 +0530

    first commit

---
+++ resources/views/layouts/frontend.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+<head>
+    <!--=============== basic  ===============-->
+    <meta charset="UTF-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'The Arch Solutions') }}</title>
+    <!--=============== css  ===============-->
+    <link type="text/css" rel="stylesheet" href="{{ asset('assets/frontend/css/plugins.css') }}">
+    <link type="text/css" rel="stylesheet" href="{{ asset('assets/frontend/css/style.css') }}">
+    <!--=============== favicons ===============-->
+    <link rel="shortcut icon" href="{{ asset('assets/frontend/images/favicon.ico') }}">
+</head>
+
+<body>
+    <!-- lodaer  -->
+    <div class="loader-wrap">
+        <div class="loader-spin"><span></span></div>
+    </div>
+    <!-- loader end  -->
+    <!--  main   -->
+    <div id="main">
+
+        @include('layouts.navigation')
+
+        <!-- Page Content -->
+        <div class="wrapper">
+            {{ $slot }}
+        </div>
+    </div>
+    <!-- Main end -->
+
+    <!--=============== scripts  ===============-->
+    <script src="{{ asset('assets/frontend/js/jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/frontend/js/plugins.js') }}"></script>
+    <script src="{{ asset('assets/frontend/js/scripts.js') }}"></script>
+</body>
+
+</html>
---
+++ resources/views/layouts/navigation.blade.php
@@ -0,0 +1,121 @@
+<!--  header-top  -->
+<div class="header-top">
+    <div class="container">
+        <a class="top-header-logo" href="index.html"></a>		
+        <!-- nav-button-wrap-->
+        <div class="nav-button-wrap">
+            <div class="nav-button">
+                <span></span><span></span><span></span>
+            </div>
+        </div>
+        <!-- nav-button-wrap end-->												
+        <div class="header-search_btn"><i class="fa fa-search transition"></i> </div>
+        <!-- lang-wrap-->
+        <div class="lang-wrap">
+            <div class="show-lang"><span><i class="fa-thin fa-language"></i><strong>En</strong></span><i class="fa fa-caret-down arrlan"></i></div>
+            <ul class="lang-tooltip lang-action no-list-style">
+                <li><a href="#" class="current-lan" data-lantext="En">English</a></li>
+                <li><a href="#" data-lantext="Fr">Franais</a></li>
+                <li><a href="#" data-lantext="Es">Espaol</a></li>
+                <li><a href="#" data-lantext="De">Deutsch</a></li>
+            </ul>
+        </div>
+        <!-- lang-wrap end--> 						
+        <div class="header-social">
+            <ul>
+                <li> <a href="#" target="_blank"><i class="fa-brands fa-facebook-f"></i></a> </li>
+                <li><a href="#" target="_blank"><i class="fa-brands fa-x-twitter"></i></a></li>
+                <li><a href="#" target="_blank"><i class="fa-brands fa-tiktok"></i></a></li>
+                <li><a href="#" target="_blank"><i class="fa-brands fa-instagram"></i></a></li>
+                <li><a href="#" target="_blank"><i class="fa-brands fa-youtube"></i></a></li>
+            </ul>
+        </div>
+        <!-- share button--> 	
+        <div class="share-button showshare">
+            <span>Share</span><i class="fa-regular fa-up-right-from-square"></i>
+        </div>
+        <!-- share button end-->											
+    </div>
+    <div class="share-holder isShare">
+        <div class="share-container"></div>
+    </div>
+    <!-- header-search-wrap -->
+    <div class="header-search-wrap">
+        <div class="widget-inner">
+            <form action="#">
+                <input name="se" id="se" type="text" class="search" placeholder="Search...">
+                <button class="search-submit" id="submit_btn"><i class="fal fa-search transition"></i> </button>
+            </form>
+        </div>
+    </div>
+    <!-- header-search-wrap end -->							
+</div>
+<!--  header-top end -->
+<!-- header -->
+<header class="main-header">
+    <div class="header-container">
+        <div class="header-inner">
+            <a href="index.html" class="logo-holder"> <img src="images/logo3.png" alt=""> </a>
+            <!--  navigation -->
+            <div class="nav-title">Menu</div>
+            <div class="nav-holder main-menu">
+                <nav class="nav-inner" id="menu-init">
+                    <ul>
+                        <li>
+                            <a href="#" class="act-link"><i class="fa-light fa-house"></i>Home</a>
+                            <!--second level -->
+                            <ul>
+                                <li><a href="index.html">Slider</a></li>
+                                <li><a href="index2.html">Carousel</a></li>
+                                <li><a href="index3.html">Slideshow</a></li>
+                                <li><a href="index4.html">Video</a></li>
+                                <li><a href="onepage.html">One Page</a></li>
+                            </ul>
+                            <!--second level end-->
+                        </li>
+                        <li><a href="about.html"><i class="fa-light fa-file-circle-info"></i>About</a></li>
+                        <li><a href="services.html"><i class="fa-light fa-trowel-bricks"></i>Services</a></li>
+                        <li>
+                            <a href="#"><i class="fa-light fa-house"></i>Projects</a>
+                            <!--second level -->
+                            <ul>
+                                <li><a href="portfolio.html">Projects</a></li>
+                                <li><a href="portfolio2.html">Projects 2</a></li>
+                                <li><a href="portfolio3.html">Projects 3</a></li>
+                                <li><a href="portfolio-single.html">Project single</a></li>
+                                <li><a href="portfolio-single2.html">Project single 2</a></li>
+                                <li><a href="portfolio-single3.html">Project single 3</a></li>
+                                <li><a href="portfolio-single4.html">Project single 4</a></li>
+                            </ul>
+                            <!--second level end-->
+                        </li>
+                        <li><a href="contact.html"><i class="fa-light fa-envelopes-bulk"></i>Contact</a></li>
+                        <li>
+                            <a href="#"><i class="fa-light fa-file-lines"></i>Pages</a>
+                            <!--second level -->
+                            <ul>
+                                <li><a href="services-single.html">Services single</a></li>
+                                <li><a href="blog-single.html">Blog single</a></li>
+                                <li><a href="coming-soon.html">Coming Soon</a></li>
+                                <li><a href="404.html">404</a></li>
+                            </ul>
+                            <!--second level end-->
+                        </li>
+                        <li><a href="blog.html"><i class="fa-light fa-credit-card-front"></i>News</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <!-- navigation  end -->
+            <div class="nav-title">Contacts</div>
+            <div class="header-contact">
+                <ul>
+                    <li><i class="fa-light fa-envelope"></i><a href="mailto:yourmail@domain.com"> yourmail@domain.com</a></li>
+                    <li><i class="fa-light fa-location-dot"></i><a href="https://goo.gl/maps/dj5UaNJFkJ6rLNJy9" target="_blank"> 27th Brooklyn New York, NY 10065</a></li>
+                    <li><i class="fa-light fa-phone-rotary"></i><a href="tel:+9(111)123456789"> +7(111)123456789</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <a href="contact.html" class="header_btn"><span>Request a Quote</span></a>
+</header>
+<!-- header end  -->
